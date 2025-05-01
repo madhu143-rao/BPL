b0e9ceb78eb6b2b2b85247a269119a4dbc5a0934
madhu143-rao <mk2429366@gmail.com>
2025-05-01 13:07:17 +0530

    Update bpl_card.java

--- bpl_card.java
+++ bpl_card.java
@@ -6,7 +6,7 @@ public class bpl_card{
     
     void display(){
         System.out.println("Your deatils are---->\nAadhaar number: "+aadhaar_card+"\nphone number: "+ph_no+"\notp: "+otp);
-        System.out.println("Your OTP is verified");
+        System.out.println("Your OTP is verified.");
         System.out.println("Your login is successfully completed.");
     }
     public static void main(String[] args){
